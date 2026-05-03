1f3f049307ef0bcf9f4818959a0d61e565565856
Emmanuel <Emmanuelceja135@outlook.com>
2026-05-02 23:23:22 -0700

    Added FullCalender ui component

---
+++ src/app/data/mockEvents.ts
@@ -0,0 +1,25 @@
+export const mockEvents = [
+    {
+        id : 1,
+        title : "Birthday",
+        date : "04-01-2026",
+        owner : "shared",
+        color : "purple",
+
+    },
+    {
+        id : 2,
+        title : "Haircut",
+        date : "04-02-2026",
+        owner : "Emmanuel",
+        color : "blue",
+    },
+    {
+        id : 3,
+        title : "nails",
+        date : "04-03-2026",
+        owner : "Crysthal",
+        color : "pink",
+        
+    }
+]
