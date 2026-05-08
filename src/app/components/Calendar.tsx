785a712b47f76c672c9ac1a954729b04aff1de2a
Emmanuel <Emmanuelceja135@outlook.com>
2026-05-07 21:21:52 -0700

    Added click event and custom header tool bar:

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -6,11 +6,36 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
 export default function Calendar() {
+    function dateClick(event) {
+        const title = prompt("Title of the Event");
+        const timeInput = prompt("Start and End Time (##:## - ##:##)");
+        const time = " Time: " + timeInput;
+        if (title && timeInput) event.view.calendar.addEvent({
+            title: title,
+            start: event.dateStr,
+            extendedProps: {
+                time: "Time: " + timeInput
+            }
+        });
+    }
     return (
         <div>
             <FullCalendar
                 plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
                 initialView={"dayGridMonth"}
+                dateClick={dateClick}
+                headerToolbar={{
+                    right: 'dayGridMonth,timeGridWeek,prev,next,today'
+                }}
+                //theme system next and go through full calendar docs
+                eventContent={(info) => {
+                    return (
+                        <div>
+                            <div>{info.event.title}</div>
+                            <div>{info.event.extendedProps.time}</div>
+                        </div>
+                    )
+                }}
             />
         </div>
     )
